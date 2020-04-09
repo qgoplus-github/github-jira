94321ef00b182369b47cfb8f24e4449f7fd3d102
Quan Go Plus <50899795+quangoplus@users.noreply.github.com>
2020-04-09 14:58:00 +0700

    Create this-is-file-test.js - Hub - Jira

---
+++ this-is-file-test.js
@@ -0,0 +1,36 @@
+//---- Here are all metrics we having ----
+
+/*
+1. Added Cyclomatic Complexity 
+2. Code Review
+3. Dependencies
+4. Dependencies Added
+5. Dependencies Removed
+6. Duplication
+7. Eslint
+8. File Dependencies
+9. Issues Resolved
+10. Lines of Code
+11. Lines of Comment
+12. Lines of Comment Changed
+13. Lines of Documentation Added
+14. Lines of Documentation Modified
+15. Lines of Documentation Removed
+16. Linked Issue Type - Bug
+17. Linked Issue Type - Feature
+18. Linked Issue Type - Mixed
+19. Linked Issue Type - Other
+20. Linked Issues
+21. LOC Churned
+22. LOC New
+23. LOC Reviewed
+24. LOC Touched
+25. LOC Written
+26. Merges
+27. Number of Changes
+28. Number of TODOs
+29. Pull Requests
+30. Related Merges
+31. Story Points
+32. Time Spent
+*/
